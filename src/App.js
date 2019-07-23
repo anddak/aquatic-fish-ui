6ee03b6791be31b165709f58e5cc7f23284332c4
Andras <andras.dako@gmail.com>
2019-07-23 11:59:20 +0100

    (feat) implemented filtering for numbers and search for text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import MainTable from "./components/MainTable";
+import SearchForm from "./components/advanced-search/search-form/SearchForm";
 import AdvancedSearch from "./components/advanced-search/AdvancedSearch";
 
 function App() {
   return (
     <div className="App">
-      <AdvancedSearch/>
+      {/*<AdvancedSearch/>*/}
+      <SearchForm/>
       <MainTable/>
     </div>
   );
