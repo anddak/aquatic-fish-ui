cf3e1bc3891625e012acb976561aec4ac142f05d
Andras <andras.dako@gmail.com>
2019-08-08 17:18:28 +0100

    (feat) completed advanced search layout

--- src/components/advanced-search/AdvancedSearch.jsx
+++ src/components/advanced-search/AdvancedSearch.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import {Form, Row, Col, Button, Icon, Input } from 'antd';
+import {Form, Row, Col, Button, Icon, Input, InputNumber, Select } from 'antd';
 import './AdvancedSearch.css';
 
+const {Option} = Select;
+
+
 /*
-Using class component because ant-design's Form requires ref's. Functional components doesn't have ref's,
+Using class component because ant-design's Form requires ref's. Functional stateless components doesn't have ref's,
 only class components.
+ */
+
+/*
+Class for the expandable advanced search section which includes ALL fish attribute fields except Description
  */
 class AdvancedSearch extends React.Component {
 
-    state = {
-        expand: false,
+    constructor () {
+        super();
+        this.state = {
+            expand: false,
+            group: ""
+        };
+        this.handleFormChange = this.handleFormChange.bind(this);
     };
 
     handleReset = () => {
@@ -21,94 +33,34 @@ class AdvancedSearch extends React.Component {
         this.setState({ expand: !expand });
     };
 
+    handleFormChange (evt) {
+        this.setState({ [evt.target.name]: evt.target.value });
+    }
+
       render() {
-          const {handleReset, handleToggle} = this;
+          const {handleReset, handleToggle, handleFormChange} = this;
           const { expand } = this.state;
+          const { getFieldDecorator } = this.props.form;
+
+          const formItemLayout = {
+              labelCol: {
+                  span: 9,
+              },
+              wrapperCol: {
+                  span: 15,
+              }
+          };
+
     return(
-        <div>
-            <Form>
-                <Row gutter={16}>
-                    <Col span={1} align="right">Name</Col>
-                    <Col span={5}>
-                        <Form.Item>
+        <div className="advanced-search-form">
+            <Form {...formItemLayout}>
+                <Row gutter={48} style={{paddingTop: 20}}>
+                    <Col offset={1} span={5} style={{display: "block"}}>
+                        <Form.Item label="Name" colon={false}>
                             <Input/>
                         </Form.Item>
                     </Col>
-                </Row>
-                <Row gutter={16}>
-                    <Col span={1} align="right">Family</Col>
-                    <Col span={5}>
-                        <Form.Item>
-                            <Input />
-                        </Form.Item>
-                    </Col>
-                    <Col span={1} align="right">Genus</Col>
-                    <Col span={5}>
-                        <Form.Item>
-                            <Input/>
-                        </Form.Item>
-                    </Col>
-                    <Col span={1} align="right">Species</Col>
-                    <Col span={5}>
-                        <Form.Item>
-                            <Input/>
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row gutter={16}>
-                    <Col span={1} align="right">Main Group</Col>
-                    <Col span={5}>
-                        <Form.Item>
-                            <Input/>
-                        </Form.Item>
-                    </Col>
-                    <Col span={1} align="right">Temperature Reference</Col>
-                    <Col span={5}>
-                        <Form.Item>
-                            <Input/>
-                        </Form.Item>
-                    </Col>
-                    <Col span={1} align="right">Breeding</Col>
-                    <Col span={5}>
-                        <Form.Item>
-                            <Input/>
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row gutter={16}>
-                    <Col span={1} align="right">Diet</Col>
-                    <Col span={5}>
-                        <Form.Item>
-                            <Input/>
-                        </Form.Item>
-                    </Col>
-                    <Col span={1} align="right">Temperament</Col>
-                    <Col span={5}>
-                        <Form.Item>
-                            <Input/>
-                        </Form.Item>
-                    </Col>
-                    <Col span={1} align="right">Level Preference</Col>
-                    <Col span={5}>
-                        <Form.Item>
-                            <Input/>
-                        </Form.Item>
-                    </Col>
-                    <Col span={1} align="right">Habitat</Col>
-                    <Col span={5}>
-                        <Form.Item>
-                            <Input/>
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>Adult Size</Col>
-                    <Col>Min/Max Temperature</Col>
-                    <Col>Water Hardness</Col>
-                    <Col>pH</Col>
-                </Row>
-                <Row>
-                    <Col style={{ textAlign: 'right' }}>
+                    <Col offset={18}>
                         <Button type="primary" htmlType="submit">
                             Search
                         </Button>
@@ -117,15 +69,109 @@ class AdvancedSearch extends React.Component {
                         </Button>
                         <a style={{ marginLeft: 8, fontSize: 12 }} onClick={handleToggle}>
                             Collapse
-                            <Icon type={expand ? 'up' : 'down'} />
+                            <Icon type={expand ? 'down' : 'up'} />
                         </a>
                     </Col>
                 </Row>
+                {expand && <div>
+                <Row gutter={48}>
+                    <Col offset={1} span={5} style={{display: "block", marginRight: 20}}>
+                        <Form.Item label="Family" colon={false}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={5} style={{display: "block", marginRight: 20}}>
+                        <Form.Item label="Genus" colon={false}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={5} style={{display: "block"}}>
+                        <Form.Item label="Species" colon={false}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row gutter={48}>
+                    <Col offset={1} span={5} style={{display: "block", marginRight: 20}}>
+                        <Form.Item label="Group" colon={false}>
+
+                            {getFieldDecorator('group')(
+                                <Select
+                                    name="group"
+                                    placeholder="Select a option"
+                                    onChange={handleFormChange}
+                                >
+                                    <Option value="marine">Marine</Option>
+                                    <Option value="freshwater">Freshwater</Option>
+                                    <Option value="pond">Pond</Option>
+                                </Select>,
+                            )}
+                        </Form.Item>
+                    </Col>
+                    <Col span={5} style={{display: "block", marginRight: 20}}>
+                        <Form.Item label="Temperature" colon={false}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={5} style={{display: "block"}}>
+                        <Form.Item label="Breeding" colon={false}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row gutter={48}>
+                        <Col offset={1} span={5} style={{display: "block", marginRight: 20}}>
+                            <Form.Item label="Diet" colon={false}>
+                                <Input/>
+                            </Form.Item>
+                        </Col>
+                    <Col span={5} style={{display: "block", marginRight: 20}}>
+                        <Form.Item label="Temperament" colon={false}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={5} style={{display: "block", marginRight: 20}}>
+                        <Form.Item label="Swim Level" colon={false}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={5} style={{display: "block"}}>
+                        <Form.Item label="Habitat" colon={false}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row gutter={48}>
+                    <Col offset={1} span={5} style={{display: "block", marginRight: 20}}>
+                        <Form.Item label="Adult Size" colon={false}>
+                            <InputNumber/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={5} style={{display: "block", marginRight: 20}}>
+                        <Form.Item label="Temperature" colon={false}>
+                            <InputNumber style={{marginRight:8}}/>
+                            <InputNumber/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={5} style={{display: "block", marginRight: 20}}>
+                        <Form.Item label="Water Hardness" colon={false}>
+                            <InputNumber style={{marginRight:8}}/>
+                            <InputNumber/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={5} style={{display: "block"}}>
+                        <Form.Item label="pH Levels" colon={false}>
+                            <InputNumber style={{marginRight:8}}/>
+                            <InputNumber/>
+                        </Form.Item>
+                    </Col>
+                </Row>
+                </div> }
             </Form>
         </div>
     )};
 }
 
-//TODO: padding for columns and more space between them, position text in the middle, maybe move search up-top
+//TODO: setup form capture, customise fields current setup is wrong because select doesn't work like input
 
 export const WrappedAdvancedSearch = Form.create({ name: 'advanced_search' })(AdvancedSearch);
